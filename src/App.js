b62e94565740d3a9d4721d96ad03c6b515991870
mark <mercercountynj@gmail.com>
2017-11-14 14:14:33 -0500

    tweet box color meter

--- src/App.js
+++ src/App.js
@@ -4,22 +4,64 @@ import './App.css';
 
 
 // Tweet Box & Counter
+class TweetBoxStatus extends React.Component {
+  render() {
+    return (
+      <textarea onChange={this.props.setStatusText} placeholder="Tweet Something" />
+    );
+  }
+}
+class TweetBoxCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.counterStyles = this.counterStyles.bind(this);
+    this.remainingCharacters = this.remainingCharacters.bind(this);
+  }
+  counterStyles() {
+    let color = 'red';
+    let remainingCharacters = this.remainingCharacters();
+    if (remainingCharacters > 30) {
+      color = 'green';
+    } else if (remainingCharacters > 0) {
+      color = 'orange';
+    }
+    return { color: color };
+  }
+  remainingCharacters() {
+    return 140 - this.props.statusText.length;
+  }
+  render() {
+    return (
+      <span style={this.counterStyles()}>{this.remainingCharacters()}</span>
+    )
+  }
+}
 class TweetBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = { statusText: '' };
     this.setStatusText = this.setStatusText.bind(this);
+    this.submitTweet = this.submitTweet.bind(this);
+    this.statusTextValid = this.statusTextValid.bind(this);
   }
   setStatusText(event) {
     let text = event.target.value;
     this.setState({ statusText: text });
+  }
+  submitTweet() {
+    if (this.statusTextValid()) {
+      this.props.tweetSubmitted(this.state.statusText);
+    }
+  }
+  statusTextValid() {
+    return this.state.statusText.length > 0 && this.state.statusText <= 140;
   }
   render() {
     return (
       <div>
         <TweetBoxStatus setStatusText={this.setStatusText} />
         <TweetBoxCounter statusText={this.state.statusText} />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={this.submitTweet} >Tweet</button>
       </div>
     );
   }
@@ -171,6 +213,7 @@ class App extends Component {
         <Clicked name="Visitor" />
         <CreditCard types={types} />
         <Prefixer />
+        <TweetBox />
       </div>
     );
   }
