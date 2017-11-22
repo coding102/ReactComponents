7976a722271255dc33f17fd0b82f9e30bd433131
Marck <mercercountynj@gmail.com>
2017-11-22 01:11:39 -0500

    list filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Grid, Row, input } from 'react-bootstrap';
+import { Table, ListGroup, ListGroupItem, Grid, Row, FormControl, Well } from 'react-bootstrap';
 import './App.css';
-import { Table } from 'react-bootstrap';
 var clone = require('clone');
 
 
 // Search function
+class ChannelSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchQuery: '' };
+    this.changeSearchQuery = this.changeSearchQuery.bind(this);
+    this.filteredChannels  = this.filteredChannels.bind(this);
+  }
+
+  changeSearchQuery(newSearchQuery) {
+    this.setState({ searchQuery: newSearchQuery });
+  }
+
+  filteredChannels() {
+    let { channels }    = this.props,
+        { searchQuery } = this.state;
+
+    return channels.filter(channel => {
+      return channel.indexOf(searchQuery) === 0;
+    });
+  }
+
+  render() {  
+    let { searchQuery } = this.state,
+        channels = this.filteredChannels();
+
+    return (
+      <Grid fluid={true}>
+        <Row>
+          <ChannelSearchInput searchQuery={searchQuery} 
+                              onSearchQueryChanged={this.changeSearchQuery} />
+          {channels.length === 0 ? 
+            <NoValidChannels searchQuery={searchQuery} /> :
+            <Channels channels={channels} />
+          }
+        </Row>
+      </Grid>
+    );
+  }  
+}
+
+class ChannelSearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this);
+  } 
+
+  handleSearchQueryChange(ev) {
+    let { onSearchQueryChanged } = this.props;
+    onSearchQueryChanged(ev.target.value)
+  }
+
+  render() {    
+    let { searchQuery } = this.props;
+
+    return (
+      <FormControl type="text" 
+         placeholder="Search channels…" 
+         bsSize="large"
+         value={searchQuery}
+         onChange={this.handleSearchQueryChange} />
+    );
+  }
+}
+
 class Channels extends React.Component {
   render() {
     let { channels } = this.props;
+
     return (
       <ListGroup>
         {channels.map(channel =>
@@ -17,38 +82,22 @@ class Channels extends React.Component {
           </ListGroupItem>
         )}
       </ListGroup>
-    );
+    );    
   }
 }
-class ChannelSearch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { searchQuery: '' };
-    this.changeSearchQuery = this.changeSearchQuery.bind(this);
-  }
-  changeSearchQuery(event) {
-    
-  }
+
+class NoValidChannels extends React.Component {
   render() {
-    let { channels } = this.props;
-    let { searchQuery } = this.state;
-    return ( 
-      <Grid fluid={true}>
-        <Row>
-            <input type="text"
-                  placeholder="Search Channels"
-                  bsSize="large" 
-                  value={searchQuery}
-                  onChange={this.changeSearchQuery} />
-            <Channels channels={channels} /> 
-        </Row>
-      </Grid>
-    );
+    let { searchQuery } = this.props;
+
+    return (<Well>There are no channels matching 
+                  your query <strong>"{searchQuery}"</strong>.
+            </Well>);
   }
 }
 ChannelSearch.defaultProps = {
   channels: [
-    "reactnative",
+    "react native",
     "api",
     "javascript",
     "ruby",
